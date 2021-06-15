30107525e72210564e27db6aac45e3c6df2d6c9f
Kawindu-Prabod <85952609+Kawindu-Prabod@users.noreply.github.com>
2021-06-15 21:16:25 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fn;
+            double sn;
+            double ans;
+            string opr;
+
+            Console.WriteLine("Calculator");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Enter First No:-");
+            fn = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Operator('+' , '-' , '*' , '/') :-");
+            opr = Console.ReadLine();
+            Console.WriteLine("Enter Second No:-");
+            sn = double.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            if(opr == "+")
+            {
+                ans = fn + sn;
+                Console.WriteLine("=" + ans);
+            }
+
+            if (opr == "-")
+            {
+                ans = fn - sn;
+                Console.WriteLine("=" + ans);
+            }
+
+            if (opr == "*")
+            {
+                ans = fn * sn;
+                Console.WriteLine("=" + ans);
+            }
+
+            if (opr == "/")
+            {
+                ans = fn / sn;
+                Console.WriteLine("=" + ans);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
